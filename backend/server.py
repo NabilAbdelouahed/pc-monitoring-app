b7bbc1349659b839557a9a6b8a5251356baa16ac
NabilAbdelouahed <nabilabdelouahed4@gmail.com>
2025-06-26 16:18:09 +0200

    test possible solutions

--- backend/server.py
+++ backend/server.py
@@ -3,8 +3,18 @@ from flask import Flask
 app = Flask("pc-monitor")
 
 @app.route("/")
-def hello_world():
+def status():
     return "<p>API is running !</p>"
+
+@app.route("/data" , methods=['POST'])
+def fetch_data():
+    return True
+
+@app.route("/login", methods=["POST"])
+def login():
+    return True
+
+
 
 if __name__ == '__main__' :
     app.run('127.0.0.1',5000)
